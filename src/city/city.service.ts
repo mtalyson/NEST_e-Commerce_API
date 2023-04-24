c9f21cb534955f9185348e56865e083b4d936069
Talyson Moreira <talyson.penha@gmail.com>
2023-04-24 17:38:49 -0300

    city controller and cache added

---
+++ src/city/city.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CityEntity } from './entities/city.entity';
+
+@Injectable()
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  async getAllCitiesByState(stateId: number): Promise<CityEntity[]> {
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
+      where: {
+        stateId,
+      },
+    });
+
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
+  }
+}
