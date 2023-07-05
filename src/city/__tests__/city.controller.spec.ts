ffa30264e64eb87cab98089484d4543c315d8b12
Talyson Moreira <talyson.penha@gmail.com>
2023-07-05 17:23:13 -0300

    city controller unit tests

---
+++ src/city/__tests__/city.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CityController } from '../city.controller';
+import { CityService } from '../city.service';
+import { cityMock } from '../__mocks__/city.mock';
+
+describe('CityController', () => {
+  let controller: CityController;
+  let cityService: CityService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: CityService,
+          useValue: {
+            getAllCitiesByStateId: jest.fn().mockResolvedValue([cityMock]),
+          },
+        },
+      ],
+      controllers: [CityController],
+    }).compile();
+
+    controller = module.get<CityController>(CityController);
+    cityService = module.get<CityService>(CityService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(cityService).toBeDefined();
+  });
+});
